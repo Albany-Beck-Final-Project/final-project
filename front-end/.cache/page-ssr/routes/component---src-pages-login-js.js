702525c5ecea101194f0085902efedda38dae206
Ben <ben.lynch1931@gmail.com>
2021-10-25 09:22:56 +0100

    Add front end to repo

---
+++ front-end/.cache/page-ssr/routes/component---src-pages-login-js.js
@@ -0,0 +1,16 @@
+exports.id = "component---src-pages-login-js";
+exports.ids = ["component---src-pages-login-js"];
+exports.modules = {
+
+/***/ "./src/pages/login.js":
+/*!****************************!*\
+  !*** ./src/pages/login.js ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
---
+++ front-end/.cache/page-ssr/routes/component---src-pages-signup-js.js
@@ -0,0 +1,16 @@
+exports.id = "component---src-pages-signup-js";
+exports.ids = ["component---src-pages-signup-js"];
+exports.modules = {
+
+/***/ "./src/pages/signup.js":
+/*!*****************************!*\
+  !*** ./src/pages/signup.js ***!
+  \*****************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
