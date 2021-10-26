1e23adf23d278345113c05bf7df5edc1c4f994e5
Ben <ben.lynch1931@gmail.com>
2021-10-26 09:37:51 +0100

    beginning tuesday

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react";
 import "../styles/global.css";
 
 import Nav from '../components/Nav';
-import {
-  main, photoDiv, about, portfolio, orders, eachOrderMain, orderTitle, eachStockMain,
-  orderId, orderDate, orderStock, orderShares, orderPrice, orderStatus, orderDirection,
-  stockName, stockShares, stockPrice, stockTitle
-} from '../styles/index.module.css';
+import { main, photoDiv, about, portfolio, orders } from '../styles/index.module.css';
+
+import { eachOrderMain, orderTitle, eachStockMain, orderId, orderDate, orderStock, orderShares, orderPrice, orderStatus,
+  orderDirection, stockName, stockShares, stockPrice, stockTitle } from '../styles/order.module.css'
+
+
+
 export default function Home() {
   if (window.localStorage.StockPlatform) {
     // USESTATE
@@ -43,7 +45,7 @@ export default function Home() {
     )
   }
 
-  if (window.localStorage.StockPlatform) {
+  // if (window.localStorage.StockPlatform) {
     return (
       <div>
         <Nav path="/"/>
@@ -93,24 +95,24 @@ export default function Home() {
         </div>
       </div>
     )
-  } else {
-    return (
-      <div>
-        <Nav path="/" />
-
-        <div className={main}>
-          <div className={photoDiv}></div>
-          <hr />
-
-          <div className={about}>
-            <h1>Stock Exchange</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus dignissim nisi vitae molestie. Proin interdum feugiat quam, non rutrum diam eleifend non. Cras consequat nunc nec erat pellentesque hendrerit. Donec ultrices urna est, eu tristique sapien pulvinar a. Sed tortor nibh, porta eget tellus quis, facilisis vulputate metus. Praesent ornare vel urna at facilisis. Cras eu eros nec ligula vulputate mollis nec at velit. Phasellus semper odio sit amet dolor blandit,    sed consequat urna gravida. Proin aliquam nisi lobortis mauris sagittis commodo. Quisque ac dolor vitae lectus pharetra posuere id vitae ex. Nulla non lacus porta augue ullamcorper efficitur. Ut accumsan eros nec metus consectetur aliquet. Nulla facilisi. Quisque egestas interdum est, non ullamcorper mi luctus at. Donec et turpis a nisl bibendum ultrices hendrerit sed nibh.
-            <br />
-            <br />
-            Integer venenatis lectus ut porta eleifend. Maecenas aliquet dolor ante. Duis quam libero, facilisis eget volutpat sit amet, tempus quis nisl. Pellentesque aliquam semper pellentesque. Suspendisse ut turpis rhoncus purus dignissim mollis ac quis sem. Donec condimentum lacus et orci mattis tincidunt. Curabitur ut auctor enim.</p>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  // } else {
+  //   return (
+  //     <div>
+  //       <Nav path="/" />
+  //
+  //       <div className={main}>
+  //         <div className={photoDiv}></div>
+  //         <hr />
+  //
+  //         <div className={about}>
+  //           <h1>Stock Exchange</h1>
+  //           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rhoncus dignissim nisi vitae molestie. Proin interdum feugiat quam, non rutrum diam eleifend non. Cras consequat nunc nec erat pellentesque hendrerit. Donec ultrices urna est, eu tristique sapien pulvinar a. Sed tortor nibh, porta eget tellus quis, facilisis vulputate metus. Praesent ornare vel urna at facilisis. Cras eu eros nec ligula vulputate mollis nec at velit. Phasellus semper odio sit amet dolor blandit,    sed consequat urna gravida. Proin aliquam nisi lobortis mauris sagittis commodo. Quisque ac dolor vitae lectus pharetra posuere id vitae ex. Nulla non lacus porta augue ullamcorper efficitur. Ut accumsan eros nec metus consectetur aliquet. Nulla facilisi. Quisque egestas interdum est, non ullamcorper mi luctus at. Donec et turpis a nisl bibendum ultrices hendrerit sed nibh.
+  //           <br />
+  //           <br />
+  //           Integer venenatis lectus ut porta eleifend. Maecenas aliquet dolor ante. Duis quam libero, facilisis eget volutpat sit amet, tempus quis nisl. Pellentesque aliquam semper pellentesque. Suspendisse ut turpis rhoncus purus dignissim mollis ac quis sem. Donec condimentum lacus et orci mattis tincidunt. Curabitur ut auctor enim.</p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
---
+++ front-end/src/pages/platform.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link, navigate } from 'gatsby';
+
+import { platform, home, homeLink, exchange, exchangeOption,  buyMain, sellMain, historyMain } from '../styles/platform.module.css';
+import { eachBuyOrder, orderId, orderDate, orderStock, orderShares, orderPrice, orderStatus, orderDirection } from '../styles/order.module.css'
+
+
+const Platform = () => {
+  // if(!(window.localStorage.StockPlatform)) {
+  //   navigate("/login");
+  // }
+
+  const renderBuyOrders = () => {
+    return (
+      <li>
+        <div className={eachBuyOrder}>
+          <div className={orderId}>1014</div>
+          <div className={orderDate}>24 OCT 2021</div>
+          <div className={orderStock}>AAPL</div>
+          <div className={orderShares}>4</div>
+          <div className={orderPrice}>£592.48</div>
+          <div className={orderStatus}>FILLED</div>
+          <div className={orderDirection} style={{ color: "#18C412" }}><strong>BUY</strong></div>
+        </div>
+      </li>
+    )
+  }
+
+  const renderSellOrders = () => {
+    return (
+      <li>
+        <div className={eachBuyOrder}>
+          <div className={orderId}>1014</div>
+          <div className={orderDate}>24 OCT 2021</div>
+          <div className={orderStock}>AAPL</div>
+          <div className={orderShares}>4</div>
+          <div className={orderPrice}>£592.48</div>
+          <div className={orderStatus}>FILLED</div>
+          <div className={orderDirection} style={{ color: "#18C412" }}><strong>SELL</strong></div>
+        </div>
+      </li>
+    )
+  }
+
+  return (
+    <div className={platform}>
+      <div className={home}>
+        <Link to="/" className={homeLink}>Home</Link>
+      </div>
+
+      <div className={exchange}>
+        <div className={exchangeOption}></div>
+
+        <div className={buyMain}>
+        {/* BUY LOW */}
+          <ul>
+            { renderBuyOrders() }
+          </ul>
+        </div>
+
+        <div className={sellMain}>
+          {/* SELL HIGH */}
+          <ul>
+            { renderSellOrders() }
+          </ul>
+        </div>
+      </div>
+      <div className={historyMain}></div>
+    </div>
+  )
+}
+
+export default Platform;
