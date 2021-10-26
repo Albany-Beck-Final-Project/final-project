fd086226fe8608b84cb40ef96b79a113b6ecb819
Ben <ben.lynch1931@gmail.com>
2021-10-26 13:53:03 +0100

    remove uneeded data about orders from /platform

--- front-end/src/components/Orders/index.js
+++ front-end/src/components/Orders/index.js
@@ -9,18 +9,16 @@ const Orders = ({ className, direction }) => {
   const renderOrders = () => {
     let rendering = [];
     let orders = direction === "BUY" ? BuyService.getOrders() : SellService.getOrders();
-    
+
     orders.forEach((order) => {
       rendering.push(
         <li>
           <div className={eachBuyOrder}>
-            <div className={orderId}>{order.id}</div>
-            <div className={orderDate}>{order.date}</div>
+            <div className={orderDirection} style={{ color: direction === "BUY" ? "#18C412" : "#FF0000" }}><strong>{order.direction}</strong></div>
             <div className={orderStock}>{order.stock}</div>
             <div className={orderShares}>{order.shares}</div>
             <div className={orderPrice}>£{order.totalPrice}</div>
-            <div className={orderStatus}>{order.status}</div>
-            <div className={orderDirection} style={{ color: "#18C412" }}><strong>{order.direction}</strong></div>
+
           </div>
         </li>
       )
