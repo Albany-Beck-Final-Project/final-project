182e515e7a007b4348193be09549811dafa7a5f2
Ben <ben.lynch1931@gmail.com>
2021-10-26 10:48:50 +0100

    decompose orders into separate components

--- front-end/src/pages/platform.js
+++ front-end/src/pages/platform.js
@@ -1,46 +1,19 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
 
-import { platform, home, homeLink, exchange, exchangeOption, buyOption, sellOption, buyMain, sellMain, historyMain } from '../styles/platform.module.css';
-import { eachBuyOrder, orderId, orderDate, orderStock, orderShares, orderPrice, orderStatus, orderDirection } from '../styles/order.module.css'
+import { platform, home, homeLink, exchange, exchangeOption, buyOption, sellOption, sellMain, historyMain } from '../styles/platform.module.css';
 
+import BuyOrders from '../components/BuyOrders';
+import SellOrders from '../components/SellOrders';
 
 const Platform = () => {
   // if(!(window.localStorage.StockPlatform)) {
   //   navigate("/login");
   // }
 
-  const renderBuyOrders = () => {
-    return (
-      <li>
-        <div className={eachBuyOrder}>
-          <div className={orderId}>1014</div>
-          <div className={orderDate}>24 OCT 2021</div>
-          <div className={orderStock}>AAPL</div>
-          <div className={orderShares}>4</div>
-          <div className={orderPrice}>£592.48</div>
-          <div className={orderStatus}>FILLED</div>
-          <div className={orderDirection} style={{ color: "#18C412" }}><strong>BUY</strong></div>
-        </div>
-      </li>
-    )
-  }
 
-  const renderSellOrders = () => {
-    return (
-      <li>
-        <div className={eachBuyOrder}>
-          <div className={orderId}>1014</div>
-          <div className={orderDate}>24 OCT 2021</div>
-          <div className={orderStock}>AAPL</div>
-          <div className={orderShares}>4</div>
-          <div className={orderPrice}>£592.48</div>
-          <div className={orderStatus}>FILLED</div>
-          <div className={orderDirection} style={{ color: "#18C412" }}><strong>SELL</strong></div>
-        </div>
-      </li>
-    )
-  }
+
+
 
   const renderExchanges = () => {
     return (null)
@@ -55,25 +28,16 @@ const Platform = () => {
       <div className={exchange}>
         <div className={exchangeOption}>
 
-        <button className={buyOption}>BUY</button>
-        { renderExchanges() }
-        <button className={sellOption}>SELL</button>
+          <button className={buyOption}>BUY</button>
+            { renderExchanges() }
+          <button className={sellOption}>SELL</button>
 
         </div>
 
-        <div className={buyMain}>
-        {/* BUY LOW */}
-          <ul>
-            { renderBuyOrders() }
-          </ul>
-        </div>
+        <BuyOrders />
 
-        <div className={sellMain}>
-          {/* SELL HIGH */}
-          <ul>
-            { renderSellOrders() }
-          </ul>
-        </div>
+        <SellOrders />
+
       </div>
       <div className={historyMain}></div>
     </div>
